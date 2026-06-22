756e27764cb58dd03ff517a93cc17fdcf0641e40
Gabriel Vitor da Silva <gabriel_silva215@estudante.sesisenai.org.br>
2026-06-22 20:34:03 -0300

    1

--- appsa/src/main/java/com/smart/appsa/repository/LaminaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.smart.appsa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.smart.appsa.Entity.Lamina;
-
-public interface LaminaRepository extends JpaRepository<Lamina, Long> {
-}
